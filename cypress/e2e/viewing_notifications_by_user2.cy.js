78b3b751fdf4cfa9fb9bffb346b80664a0cb4e03
EvaKolesnikova <evakoless@gmail.com>
2025-09-09 22:16:08 +0700

    viewing_user_notifications2

---
+++ cypress/e2e/viewing_notifications_by_user2.cy.js
@@ -0,0 +1,16 @@
+describe('Авторизация пользователя', () => {
+    it('Авторизация пользователя', () => {
+        cy.visit('https://dev.profteam.su/login');
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(1) > div.form-control--medium.form-control > input')
+            .click()
+            .type('Eva');
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(2) > div.form-control--medium.form-control > input')
+            .click()
+            .type('QWEasd123');
+        cy.get('#app > div.page > div > section > form > div.form__buttons > div:nth-child(3) > button')
+            .click();
+        cy.get('#app > div.page > header:nth-child(1) > nav > a:nth-child(4) > span').click();
+
+
+    });
+});
