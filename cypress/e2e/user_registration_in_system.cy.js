3979801a9d7ef8877fcc3fafab35fab23dd0a5f6
EvaKolesnikova <evakoless@gmail.com>
2025-09-09 15:07:53 +0700

    registration

---
+++ cypress/e2e/user_registration_in_system.cy.js
@@ -0,0 +1,44 @@
+describe('Регистрация пользователя в системе', () => {
+    it('Регистрация пользователя в системе', () => {
+        cy.visit('https://dev.profteam.su/registration');
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(1) > div:nth-child(1) > div > input')
+            .click()
+            .type('Eva');
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(1) > div:nth-child(2) > div > input')
+            .click()
+            .type('qwe411463@gmail.com');
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(1) > div:nth-child(3) > div > input')
+            .click()
+            .type('QWEasd123');
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(1) > div:nth-child(4) > div > input')
+            .click()
+            .type('QWEasd123');
+
+        cy.get('#app > div.page > div > section > form > div.form__buttons > div:nth-child(4) > button').click();
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(2) > div:nth-child(1) > div > input')
+            .click()
+            .type('Колесникова');
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(2) > div:nth-child(2) > div > input')
+            .click()
+            .type('Ева');
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(2) > div:nth-child(3) > div > input')
+            .click()
+            .type('Сергеевна');
+
+        cy.get('#app > div.page > div > section > form > div:nth-child(1) > div.form__labels > div:nth-child(2) > div:nth-child(4) > div > input')
+            .click();
+
+        cy.get('input[type="file"]')
+            .attachFile('Ava.jpg', { subjectType: 'input' });
+
+        cy.get('#app > div.page > div > section > form > div.form__buttons > div:nth-child(3) > button').click();
+
+    });
+});
